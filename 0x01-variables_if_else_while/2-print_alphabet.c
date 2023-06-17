a550d22ad5f4688b850043368b2ad0fe0e24eb89
wallacesintra <wallvxwallace962@gmail.com>
2023-06-17 05:15:13 -0700

    print alphabets with putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 /** prints alphabets */
 int main(void){
-	putchar("abcdefghijklmnopqrstuvwxyz");
+	char alphabets[26]= "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	for (i = 0;i < 26;i++) {
+		putchar(alphabets[i]);
+	}
+	putchar("\n");
 	return 0;
 }
