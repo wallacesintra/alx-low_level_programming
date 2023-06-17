ea19bcab3f3560254ed9907467e4e00dcd28300c
wallacesintra <wallvxwallace962@gmail.com>
2023-06-17 08:58:09 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print reverse
+ *
+ * return - always 0 (success)
+ */
+int main(void)
+{
+	char revAlp[26] = "zyxwvutsrqponmlkjihgfedcba";
+
+	int i;
+
+	for (i = 26;i = 0;i--)
+	{
+		putchar(revAlp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
