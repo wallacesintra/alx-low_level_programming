5d8cc30df6731e9543119f713a41cbca5a15e238
wallacesintra <wallvxwallace962@gmail.com>
2023-06-29 14:13:29 -0700

    mandatory

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * @dest: destination.
  * @src: source.
  * @n: amount of bytes used from src.
- * Return- the pointer to dest
+ * Return: the pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
