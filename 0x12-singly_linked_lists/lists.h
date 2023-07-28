9c6a55a09d234d74661d8163a89977bb7c7a59b9
wallace, the developer <103658056+wallacesintra@users.noreply.github.com>
2023-07-29 02:10:54 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @nextPtr: points to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
@@ -18,7 +18,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *nextPtr;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
