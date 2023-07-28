4a43531ece2464a3949fe45b4ffaed1b97ad8e8d
wallacesintra <wallvxwallace962@gmail.com>
2023-07-28 15:08:08 -0700

    mandatory tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t no;
+
+	no = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		no++;
+	}
+	return (no);
+}
