b9c74a591add7f971b83d881231f48de01d3d6d5
wallacesintra <wallvxwallace962@gmail.com>
2023-08-01 14:15:03 -0700

    mandatory task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a singly linked list
+ * @head: head of a list.
+ * Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
