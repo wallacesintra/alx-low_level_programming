39987f346beeb860830668e69dc13a7a12d38a80
wallacesintra <wallvxwallace962@gmail.com>
2023-09-12 07:47:28 -0700

    initilize struct dog function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (p)
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
